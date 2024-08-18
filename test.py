8c063c04eebe542a53615e8b63a60b1947760730
Mubi <ch.mubasher.ikram@gmail.com>
2024-08-19 00:51:28 +0500

    testing message

--- test.py
+++ test.py
@@ -1,20 +1,5 @@
 print("Hello")
-
-print(2/3)
-print("This is a divide function")
-
-
-print(2+3)
-print("This is add function")
-
-print(2*3)
-print("This is multiply function")
-
-print(2-3)
-print("This is minus function")
-
-print("Hello")
-print("Hello 2")
+print("Everything is reset now")
 
 
 
